a132770eca4677a111108b69c74c77582a8948a7
Nicolas SERVAIS <nicolasservais@icloud.com>
2021-09-13 13:41:32 +0200

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Nicolas SERVAIS on 13/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
